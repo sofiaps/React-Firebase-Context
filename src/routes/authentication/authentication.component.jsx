2a99e988f1c058443452bd0f85c1182ad685c442
sofiaps <31549943+sofiaps@users.noreply.github.com>
2023-01-27 12:21:45 +0100

    Update authentication.component.jsx

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,9 +1,19 @@
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 import SignInForm from "../../components/sign-in-form/sign-in-form.component";
+import { useContext, useEffect } from "react";
+import { UserContext } from "../../contexts/user.context";
 
 import "./authentication.styles.scss";
+import { useNavigate } from "react-router-dom";
 
 const Authentication = () => {
+  const { currentUser } = useContext(UserContext);
+  const navigate = useNavigate();
+  useEffect(()=>{
+    if(currentUser) {
+      return navigate("/");
+    }
+  }, [currentUser, navigate])
   return (
     <div className="authentication-container">
       <SignInForm />

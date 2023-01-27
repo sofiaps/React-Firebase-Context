fe8fc5d0149cecabcbe82f61491e273c3350388b
sofiaps <31549943+sofiaps@users.noreply.github.com>
2023-01-27 13:40:48 +0100

    Update shop.component.jsx

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,26 +1,12 @@
-import { Fragment, useContext } from "react";
-
-import ProductCard from "../../components/product-card/product-card.component";
-
-import { CategoriesContext } from "../../contexts/categories.context";
+import { Outlet } from "react-router-dom"
 
 import "./shop.styles.scss";
 
 const Shop = () => {
-  const { categoriesMap } = useContext(CategoriesContext);
 
   return (
     <div className="shop-container">
-      {Object.keys(categoriesMap).map((title) => (
-        <Fragment key={title}>
-          <h2>{title}</h2>
-          <div className="products-container">
-            {categoriesMap[title].map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-          </div>
-        </Fragment>
-      ))}
+      <Outlet/>
     </div>
   );
 };
